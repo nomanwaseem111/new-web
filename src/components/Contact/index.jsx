cf2b9d597cda394816c98fdc39bd7e77b6e6a39c
Noman Waseem <nomanwaseem111@gmail.com>
2022-12-11 05:55:38 +0500

    few changes

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
 import WifiCalling3Icon from '@mui/icons-material/WifiCalling3';
 import Typography from '@mui/material/Typography';
+import PublicIcon from '@mui/icons-material/Public';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import TextField from '@mui/material/TextField';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+
 import {
 
   Link
@@ -18,49 +25,72 @@ const Contact = () => {
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3620.0094933385203!2d67.05912091409715!3d24.86352535126005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33f3395a5ca43%3A0xbb199abfe2db29e!2sTech%20Matter%20(Pvt)%20Ltd.!5e0!3m2!1sen!2s!4v1670027099671!5m2!1sen!2s" border="none" allowfullscreen="" loading="lazy" height="800px" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </Stack>
       <Stack width="85%" margin="auto" mt="20px" height="700px" direction="row">
-        <Stack width="35%" border="1px solid black">
-          <Box width="70%" margin="auto"  height="600px" >
+        <Stack width="40%" backgroundColor="#f3f3f3">
+          <Box width="80%" margin="auto" pt="30px" height="600px" >
 
-            <Box  margin="auto" mt="50px" height="60px" justifyContent="space-around" display="flex" width="300px">
-              <WifiCalling3Icon fontSize='40px' margin="auto" sx={{ width: "50px", height: "50px" }} />
-              <Box width="200px"  height="50px">
-                <Typography>+012 345 678 102</Typography>
-                <Typography>+012 345 678 102</Typography>
-
-              </Box>
-            </Box>
-
-            <Box  margin="auto" mt="50px" height="60px" justifyContent="space-around" display="flex" width="300px">
-              <WifiCalling3Icon fontSize='40px' margin="auto" sx={{ width: "50px", height: "50px" }} />
-              <Box width="200px"  height="50px">
-                <Typography>+012 345 678 102</Typography>
-                <Typography>+012 345 678 102</Typography>
+            <Box margin="auto" mt="50px" height="60px" justifyContent="space-around" display="flex" width="300px">
+              <WifiCalling3Icon margin="auto" sx={{ width: "55px", color: "black", background: "#f3f3f3", height: "55px", padding: "10px", "&:hover": { backgroundColor: "black", color: "#fff" }, border: "1px solid black", borderRadius: "50%" }} />
+              <Box width="350px" padding="5px" ml="20px" height="60px">
+                <Typography fontSize="20px" fontFamily="sans-serif" width="350px" variant="p">+012 345 678 102 <br />+012 345 678 102</Typography>
 
               </Box>
             </Box>
 
-            <Box  margin="auto" mt="50px" height="60px" justifyContent="space-around" display="flex" width="300px">
-              <WifiCalling3Icon fontSize='40px' margin="auto" sx={{ width: "50px", height: "50px" }} />
-              <Box width="200px"  height="50px">
-                <Typography>+012 345 678 102</Typography>
-                <Typography>+012 345 678 102</Typography>
+            <Box margin="auto" mt="50px" height="60px" justifyContent="space-around" display="flex" width="300px">
+              <PublicIcon margin="auto" sx={{ width: "55px", color: "black", background: "#f3f3f3", height: "55px", padding: "10px", "&:hover": { backgroundColor: "black", color: "#fff" }, border: "1px solid black", borderRadius: "50%" }} />
+              <Box width="350px" padding="5px" ml="20px" height="60px">
+                <Typography fontSize="20px" fontFamily="sans-serif" width="350px" variant="p">yourname@email.com <br />yourwebsitename.com</Typography>
 
               </Box>
             </Box>
 
-            <Box  margin="auto" mt="50px" height="60px" justifyContent="space-around" display="flex" width="300px">
-              <WifiCalling3Icon  margin="auto"   sx={{ width: "50px", color:"#fff",background:"black", height: "50px" ,padding:"10px",fontSize:'10px',border:"1px solid black",borderRadius:"50%"}} />
-              <Box width="200px"  height="50px">
-                <Typography>+012 345 678 102</Typography>
-                <Typography>+012 345 678 102</Typography>
+            <Box margin="auto" mt="50px" height="60px" justifyContent="space-around" display="flex" width="300px">
+              <LocationOnIcon margin="auto" sx={{ width: "55px", color: "black", background: "#f3f3f3", height: "55px", padding: "10px", "&:hover": { backgroundColor: "black", color: "#fff" }, border: "1px solid black", borderRadius: "50%" }} />
+              <Box width="350px" padding="5px" ml="20px" height="60px">
+                <Typography fontSize="20px" fontFamily="sans-serif" width="350px" variant="p">Address goes here,<br />street, Crossroad 123.</Typography>
 
               </Box>
             </Box>
+            <Box margin="auto" mt="50px" height="150px" width="300px">
+              <Typography variant="h4" textAlign="center" >Follow Us</Typography>
+              <Box mt="30px" width="300px" textAlign="center" justifyContent="space-evenly" display="flex">
+                <Typography component={Link} fontSize="22px" color="grey" to="https://www.facebook.com" sx={{ "&:hover": { color: "#6610f2" } }}><i class="fa-brands fa-facebook-f"></i></Typography>
+                <Typography component={Link} fontSize="22px" color="grey" to="www.facebook.com" sx={{ "&:hover": { color: "#6610f2" } }} ><i class="fa-brands fa-pinterest-p"></i></Typography>
+                <Typography component={Link} fontSize="22px" color="grey" to="www.facebook.com" sx={{ "&:hover": { color: "#6610f2" } }}><i class="fa-brands fa-tumblr"></i></Typography>
+                <Typography component={Link} fontSize="22px" color="grey" to="www.facebook.com" sx={{ "&:hover": { color: "#6610f2" } }}><i class="fa-brands fa-vimeo-v"></i></Typography>
+                <Typography component={Link} fontSize="22px" color="grey" to="www.facebook.com" sx={{ "&:hover": { color: "#6610f2" } }}><i class="fa-brands fa-twitter"></i></Typography>
+
+              </Box>
+            </Box>
+
 
 
           </Box>
         </Stack >
-        <Stack width="70%" ml="40px" border="1px solid red">
+
+
+
+
+        <Stack width="75%" ml="15px" backgroundColor="#f3f3f3">
+          <Stack width="80%" height="550px"  margin="auto">
+            <Typography variant="h4" px="49px" color="#464646" fontWeight="600">Get in Touch</Typography>
+            <Stack direction="row" justifyContent="space-evenly" alignItems="center" mt="50px">
+              <TextField id="outlined-basic" label="Name" variant="outlined" sx={{ width: "320px" }} />
+              <TextField id="outlined-basic" label="Email" variant="outlined" sx={{ width: "320px" }} />
+
+            </Stack>
+            <TextField id="outlined-basic" label="Subject" variant="outlined" sx={{ width: "88%", mx: "49px", mt: "40px" }} />
+            <TextField placeholder='Enter your Message'
+              variant='outlined'
+              label="Enter your Message"
+              multiline
+              rows={5}
+
+              sx={{ width: "88%", mx: "49px", mt: "40px" }}
+            />
+            <Button className='shopNow' sx={{ width: "150px", mt: "40px", backgroundColor:"#404040", ml: "50px", padding: "10px", color: "#fff", fontSize: "18px", "&:hover": { background: "#6610f2", border: "none", color: "#fff", fontWeight: "bold" } }}>Send</Button>
+
+          </Stack>
 
         </Stack>
       </Stack>
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 import Stack from "@mui/material/Stack"
 import { Typography } from "@mui/material"
-import  Container  from "@mui/material/Container"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
 
 
 const Footer = () => {
   return (
    
      
-      <Stack direction="row" justifyContent="space-around" mt="150px"  backgroundColor="#f0f2f5" alignItems="center" flexWrap="wrap" sx={{height:"550px",px:"8%"}}>
+      <Stack direction="row" justifyContent="space-around" mt="150px"  backgroundColor="#f6f6f8" alignItems="center" flexWrap="wrap" sx={{height:"500px",px:"3%"}}>
         <Stack>
           <img
             src="https://flone.jamstacktemplates.dev/assets/img/logo/logo.png"
@@ -17,8 +18,8 @@ const Footer = () => {
           />
           <Typography sx={{fontSize:"20px", mt:"20px", letterSpacing:"1px"}}>© 2022 Flone.<br/> All Rights Reserved</Typography>
         </Stack>
-        <Stack spacing={2} sx={{padding:"10px"}}>
-            <Typography component="h2" sx={{fontSize:"22px", fontWeight:"400" , textTransform:"uppercase"}}>About Us</Typography>
+        <Stack spacing={2} sx={{padding:"10px" ,height:"300px"}}>
+            <Typography component="h2" sx={{fontSize:"20px", fontWeight:"480" ,lineHeight:"45px",fontFamily:"Poppins,sans-serif", textTransform:"uppercase", color:"#313131"}}>About Us</Typography>
             <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>About us</Typography>
             <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Store location</Typography>
             <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Contact</Typography>
@@ -26,32 +27,31 @@ const Footer = () => {
 
         </Stack>
 
-        <Stack spacing={2} sx={{padding:"10px"}}>
-            <Typography component="h2" sx={{fontSize:"22px", fontWeight:"400" , textTransform:"uppercase"}}>USEFUL LINKS</Typography>
+        <Stack spacing={2} sx={{padding:"10px",height:"300px"}}>
+            <Typography component="h2" sx={{fontSize:"20px", fontWeight:"480" ,lineHeight:"45px",fontFamily:"Poppins,sans-serif" , textTransform:"uppercase", color:"#313131"}}>USEFUL LINKS</Typography>
             <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Returns</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Store location</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Contact</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Orders tracking</Typography>
+            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Support Policy</Typography>
+            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Size guide</Typography>
+            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>FAQs</Typography>
 
         </Stack>
 
 
-        <Stack spacing={2} sx={{padding:"10px"}}>
-            <Typography component="h2" sx={{fontSize:"22px", fontWeight:"400" , textTransform:"uppercase"}}>About Us</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>About us</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Store location</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Contact</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Orders tracking</Typography>
+        <Stack spacing={2} sx={{padding:"10px",height:"300px"}}>
+            <Typography component="h2" sx={{fontSize:"20px",fontWeight:"480" ,lineHeight:"45px",fontFamily:"Poppins,sans-serif", textTransform:"uppercase", color:"#313131"}}>FOLLOW US</Typography>
+            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Facebook</Typography>
+            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Twitter</Typography>
+            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Instagram</Typography>
+            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Youtube</Typography>
 
         </Stack>
 
 
-        <Stack spacing={2} sx={{padding:"10px"}}>
-            <Typography component="h2" sx={{fontSize:"22px", fontWeight:"400" , textTransform:"uppercase"}}>About Us</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>About us</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Store location</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Contact</Typography>
-            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Orders tracking</Typography>
+        <Stack spacing={2} sx={{padding:"10px", width:"400px",height:"300px"}}>
+            <Typography component="h2" sx={{fontSize:"20px", fontWeight:"480" ,lineHeight:"45px",fontFamily:"Poppins,sans-serif" , textTransform:"uppercase", color:"#313131"}}>SUBSCRIBE</Typography>
+            <Typography component="p" sx={{fontSize:"20px", fontWeight:"400", color:"#5d5d5d", }}>Get E-mail updates about our latest shop and special offers</Typography>
+            <TextField id="standard-basic" label="Enter Your Email Address" variant="standard"  />
+            <Button sx={{ width: "150px", mt: "40px", backgroundColor:"#6610f2", ml: "50px", padding: "10px", color: "#fff", fontSize: "18px", "&:hover": { background: "#f0f2f5", border: "1px solid #6610f2", color: "#6610f2"} }}>SUBSCRIBE</Button>
 
         </Stack>
        
