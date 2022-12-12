080152593ca35718cfd6e5285f202771aabf1b89
rafaekhatri11 <rafaekhatri11@gmail.com>
2022-12-12 20:54:35 +0500

    responsive

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,33 +1,77 @@
-import React from 'react'
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
-import Button from '@mui/material/Button';
+import React from "react"
+import Stack from "@mui/material/Stack"
+import Box from "@mui/material/Box"
+import { Typography } from "@mui/material"
+import Button from "@mui/material/Button"
+import Container from "@mui/material/Container"
+import Grid from "@mui/material/Grid"
+import { height, lineHeight } from "@mui/system"
 
 const Banner = () => {
-    return (
-        <>
-           <Stack backgroundColor="#f0e0ff">
-            <Stack direction="row"
-                alignItems="center"
-                width="100%"
-                // backgroundColor="yellow"
-               justifyContent="space-between"
-                px="100px"
+  return (
+    <Grid sx={{ height:{md:"auto"} ,backgroundColor:{md:"#f0e0ff",xs:"#f0e0ff"}}}>
+      <Container maxWidth="xl"  >
+        <Stack sx={{ width: { md: "100%", sm: "100%", lg: "100%" } ,height:{xs:"535px",md:"760px"} }}>
+          <Stack
+            direction={{ md: "row", xs: "column" }}
+            m={{ md: "20px" }}
+            textAlign={{ md: "start", xs: "center" }}
+            mt={{xs:"30px"}}
+          >
+            <Box
+              sx={{
+                width: { md: "100%", xs: "100%" },
+                height: { md: "400px" },
+                mt:{md:"200px"}
                 
-                >
-                <Box sx={{height:'500px',width:"900px"}}>
-                    <Typography variant='span' sx={{ml:"5px", height:"100px" , fontSize:"30px", fontWeight:"bold",fontFamily:"sans-serif"}}>Smart Products</Typography>
-                    <Typography variant='h1' sx={{mt:"20px",mb:"20px", fontSize:"110px" ,fontWeight:"400"}}>Winter Offer <br /> 2024 Collection</Typography>
-                    <Button className='shopNow' sx={{ border: "1px solid black", width: "250px",mt:"30px", ml:"10px", padding:"20px", color:"black", fontSize:"20px", "&:hover":{background:"#6610f2", border:"none", color:"#fff", fontWeight:"bold" }}}>Shop Now</Button>
-                </Box>
-                <Box sx={{mt:"80px" ,ml:"50px", width:"800px"}}>
-                    <img  src="https://flone.jamstacktemplates.dev/assets/img/slider/single-slide-hm1-2.png" height="900px" width="auto"  alt="" />
-                </Box>
-            </Stack>
-            </Stack>
-        </>
-    )
+              }}
+            >
+              <Typography
+                variant="span"
+                sx={{ fontSize: { lg: "30px", md: "20", xs: "14px" } ,lineHeight:{xs:"2px"} }}
+              >
+                Smart Products
+              </Typography>
+              <Typography
+                variant="h1"
+                sx={{
+                  fontSize: { lg: "90px", md: "80px", xs: "40px" },
+                  fontWeight: { lg: "600", md: "300", xs: "700" },
+                }}
+              >
+                Winter Offer <br /> 2024 Collection
+              </Typography>
+              <Button
+                className="shopNow"
+                sx={{ width: { md: "220px", xs: "120px" },
+                   mt:{md:"30px",xs:"10px"},
+                   ml:{md:"10px",md:"5px"},
+                   border:{md:"1px solid black",xs:"1px solid black"},
+                   padding:{md:"10px",xs:"10px"},
+                   color:{md:"black",xs:"black"},
+                   fontSize:{md:"20px",xs:"14px"},
+                   
+                 }}
+              >
+                Shop Now
+              </Button>
+            </Box>
+            <Box
+              component="img"
+              src="https://flone.jamstacktemplates.dev/assets/img/slider/single-slide-hm1-2.png"
+              sx={{
+                width: { md: "700px", xs: "300px" },
+                
+                m: { xs: "auto" },
+                mt:{xs:"40px"},
+                
+              }}
+            />
+          </Stack>
+        </Stack>
+      </Container>
+    </Grid>
+  )
 }
 
 export default Banner
--- src/components/Offer/index.jsx
+++ src/components/Offer/index.jsx
@@ -6,69 +6,88 @@ import { Typography } from "@mui/material"
 const index = () => {
   return (
     <>
-      <Stack
-        direction="row"
-        justifyContent="space-around"
-        alignItems="center"
-        width="90%"
-        mt="30px"
-        margin="auto"
-        height="250px"
-        flexWrap="wrap"
-        mb="50px"
-        py="50px"
-      >
-        <Box sx={{display:"flex", width:"350px",px:"20px",mt:"30px"}} >
+      <Stack direction={{ md: "row", xs: "column" }}>
+        <Box sx={{ display: "flex", width: "350px", px: "20px", mt: "30px" }}>
           <img
             src="https://flone.jamstacktemplates.dev/assets/img/icon-img/support-1.png"
             alt=""
-            height="65px"
+            sx={{ height: { md: "100px" }, 
+            
+             }}
           />
-          <Box sx={{ml:"20px",mt:"2px"}}>
-            <Typography component="span" sx={{fontSize:"25px"}}>Free Shipping</Typography><br/>
-            <Typography component="span" sx={{fontSize:"18px", color:"grey"}}>Free shipping on all order</Typography>
+          <Box sx={{ ml: "20px", mt: "2px" }}>
+            <Typography component="span" sx={{ fontSize: "25px" }}>
+              Free Shipping
+            </Typography>
+            <br />
+            <Typography
+              component="span"
+              sx={{ fontSize: "18px", color: "grey" }}
+            >
+              Free shipping on all order
+            </Typography>
           </Box>
         </Box>
 
-        <Box sx={{display:"flex", width:"350px",px:"20px",mt:"30px"}} >
+        <Box sx={{ display: "flex", width: "350px", px: "20px", mt: "30px" }}>
           <img
             src="https://flone.jamstacktemplates.dev/assets/img/icon-img/support-2.png"
             alt=""
             height="65px"
           />
-          <Box sx={{ml:"20px",mt:"2px"}}>
-            <Typography component="span" sx={{fontSize:"25px"}}>Support 24/7</Typography><br/>
-            <Typography component="span" sx={{fontSize:"18px", color:"grey"}}>Free shipping on all order</Typography>
+          <Box sx={{ ml: "20px", mt: "2px" }}>
+            <Typography component="span" sx={{ fontSize: "25px" }}>
+              Support 24/7
+            </Typography>
+            <br />
+            <Typography
+              component="span"
+              sx={{ fontSize: "18px", color: "grey" }}
+            >
+              Free shipping on all order
+            </Typography>
           </Box>
         </Box>
 
-        <Box sx={{display:"flex", width:"350px",px:"20px",mt:"30px"}} >
+        <Box sx={{ display: "flex", width: "350px", px: "20px", mt: "30px" }}>
           <img
             src="https://flone.jamstacktemplates.dev/assets/img/icon-img/support-3.png"
             alt=""
             height="65px"
           />
-          <Box sx={{ml:"20px",mt:"2px"}}>
-            <Typography component="span" sx={{fontSize:"25px"}}>Money Return</Typography><br/>
-            <Typography component="span" sx={{fontSize:"18px", color:"grey"}}>Free shipping on all order</Typography>
+          <Box sx={{ ml: "20px", mt: "2px" }}>
+            <Typography component="span" sx={{ fontSize: "25px" }}>
+              Money Return
+            </Typography>
+            <br />
+            <Typography
+              component="span"
+              sx={{ fontSize: "18px", color: "grey" }}
+            >
+              Free shipping on all order
+            </Typography>
           </Box>
         </Box>
 
-
-        <Box sx={{display:"flex", width:"350px",px:"20px",mt:"30px"}} >
+        <Box sx={{ display: "flex", width: "350px", px: "20px", mt: "30px" }}>
           <img
             src="https://flone.jamstacktemplates.dev/assets/img/icon-img/support-4.png"
             alt=""
             height="65px"
-            
-            
           />
-          <Box sx={{ml:"20px",mt:"2px"}}>
-            <Typography component="span" sx={{fontSize:"25px"}}>Order Discount</Typography><br/>
-            <Typography component="span" sx={{fontSize:"18px", color:"grey"}}>Free shipping on all order</Typography>
+          <Box sx={{ ml: "20px", mt: "2px" }}>
+            <Typography component="span" sx={{ fontSize: "25px" }}>
+              Order Discount
+            </Typography>
+            <br />
+            <Typography
+              component="span"
+              sx={{ fontSize: "18px", color: "grey" }}
+            >
+              Free shipping on all order
+            </Typography>
           </Box>
         </Box>
-      
       </Stack>
     </>
   )
