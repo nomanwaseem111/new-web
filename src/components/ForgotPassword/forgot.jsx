ed448095b8ec01b7565d51aec626e9287836fad2
rafaekhatri11 <rafaekhatri11@gmail.com>
2022-12-14 20:49:18 +0500

    final commit

--- src/components/ForgotPassword/forgot.jsx
+++ src/components/ForgotPassword/forgot.jsx
@@ -9,9 +9,9 @@ import Container from '@mui/material/Container';
 const forgot = () => {
   return (
  
-    <Grid sx={{backgroundColor: { md: "white", xs: "white",sm:"grey" }}}>
-    <Container maxWidth="xl"   sx={{display:{md:"flex",xs:"flex"},width:{md:"auto",xs:"auto"}}}>
-    <Stack  display="flex" justifyContent="center" ml={{md:"400px",xs:"none"}} mt="200px" sx={{width:{md:"650px",xs:"350px"},height:{md:"370px",xs:"auto"}, padding:{md:"40px",xs:"15px"},backgroundColor:{md:"#f3f3f3",xs:"#f3f3f3"}, borderRadius:{md:"20px",xs:"10px"},paddingTop:{xs:"30px",md:"50px"}}}>
+    <Grid sx={{backgroundColor: { md: "grey", xs: "grey",sm:"grey" }}}>
+    <Container maxWidth="xl"   sx={{display:{md:"flex",xs:"flex"},width:{md:"auto",xs:"auto"},height:{md:"800px",xs:"600px"}}}>
+    <Stack  display="flex" justifyContent="center" ml={{md:"400px",xs:"none"}} mt="200px" sx={{width:{md:"650px",xs:"350px"},height:{md:"370px",xs:"26s0px"}, padding:{md:"40px",xs:"15px"},backgroundColor:{md:"#f3f3f3",xs:"#f3f3f3"}, borderRadius:{md:"20px",xs:"10px"},paddingTop:{xs:"30px",md:"50px"}}}>
           <Typography variant="h5" sx={{mb:{md:"20px",xs:"10px"}, fontSize:{md:"24px",xs:"16px"}}} >Find Your Account</Typography>
           <hr/>
           <Typography  variant="p" sx={{fontSize:{md:"20px",xs:"14px"},mb:{md:"20px",xs:"10px"},mt:{md:"25px",xs:'18px'},fontFamily:{md:"sans-serif",xs:"sans-serif"}}} >Please enter your email address or mobile number to search for your account.</Typography>
